d5c44bd712b8f74b71db5a328e515f94adf61393
EllyWooo <30033719@student.toiohomai.ac.nz>
2023-08-30 22:41:24 +1200

    change

--- src/Scp.js
+++ src/Scp.js
@@ -6,7 +6,7 @@ function Scp ({scp})
         <div className="component container">
             <h1>{scp.Item}</h1>
             <h2>Class: {scp.Class}</h2>
-            <img src= {scp.Image} alt=""/> 
+            <img className="box" src= {scp.Image} alt=""/> 
             <h3>Special Containment Procedures:</h3>
             <p>{scp.Special_Containment_Procedures}</p>
             <h3>Description:</h3>
